05aa6b445d03acd5072a4cbb6d08e95f439df009
Budi Oktaviyan <budioktaviyans@gmail.com>
2019-10-11 18:54:42 +0700

    Initial constraint layout

---
+++ app/src/main/java/id/kotlin/belajar/DetailActivity.kt
@@ -0,0 +1,12 @@
+package id.kotlin.belajar
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DetailActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_detail)
+  }
+}
