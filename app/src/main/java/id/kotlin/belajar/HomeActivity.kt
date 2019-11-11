1ab72baf43e826635a69d77a1be9f5203041c38e
Budi Oktaviyan <budioktaviyans@gmail.com>
2019-11-11 18:08:28 +0700

    Initial basic activity navigation

--- app/src/main/java/id/kotlin/belajar/HomeActivity.kt
+++ app/src/main/java/id/kotlin/belajar/HomeActivity.kt
@@ -1,9 +1,8 @@
 package id.kotlin.belajar
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
@@ -11,13 +10,13 @@ class HomeActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_home)
-
-    val viewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
-    viewModel.name.postValue("Budi Oktaviyan")
-
-    val nameObserver = Observer<String> { name ->
-      tv_home.text = name
+    btn_open.setOnClickListener {
+      startActivity(
+          Intent(
+              this,
+              DetailActivity::class.java
+          )
+      )
     }
-    viewModel.name.observe(this, nameObserver)
   }
 }
